611c1b7b889e0d4e17504717270b75b7d273c9bd
ricardo <ricardodeveloper03@gmail.com>
2024-09-21 14:32:19 +0200

    fix

--- src/data/getCatData.ts
+++ src/data/getCatData.ts
@@ -1,44 +1,48 @@
-import { db } from '@/data/db'
 export const prerender = false
-
-const connection = await db.getConnection()
+import { db } from '@/data/db'
 
 export async function getAllDailiesCats() {
+  const connection = await db.getConnection()
   try {
     const [rows] = await connection.query('SELECT * FROM daily_cat')
-
     return rows
   } catch (error) {
     console.log('getAllDailiesCats: ', error)
     return 'Something went wrong'
+  } finally {
+    connection.release()
   }
 }
 
 export async function getOneDailyCatById(id: number) {
+  const connection = await db.getConnection()
   try {
     const [rows] = await connection.query(
       'SELECT * FROM daily_cat WHERE id = ?',
       [id]
     )
-
     // @ts-ignore
     return rows[0]
   } catch (error) {
     console.log('getOneDailyCatById: ', error)
     return 'Something went wrong'
+  } finally {
+    connection.release()
   }
 }
 
 export async function getLastCatId() {
+  const connection = await db.getConnection()
   try {
     const [rows] = await connection.query(
       'SELECT id FROM daily_cat ORDER BY id DESC LIMIT 1'
     )
-
     // @ts-ignore
-    return rows[0].id
+    return rows[0]?.id
   } catch (error) {
     console.log('getLastCatId: ', error)
     return 'Something went wrong'
+  } finally {
+    connection.release()
   }
 }
