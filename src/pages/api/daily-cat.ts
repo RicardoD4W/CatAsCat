190b49a9d7cec145e6b4a6b54fa2b70a1b392301
ricardo <ricardodeveloper03@gmail.com>
2024-10-18 17:51:27 +0200

    redirects

--- src/pages/api/daily-cat.ts
+++ src/pages/api/daily-cat.ts
@@ -34,10 +34,10 @@ export async function getCatImageOfTheDay(clientDate: string) {
       let secondDate = getCurrentDateTime(aux).split(' ')[0]!.split('-').at(-1)
 
       if (firstDate! > secondDate!) {
-        return new Response(null, {
+        return {
           status: 307,
           headers: { Location: '/your-cat-daily' },
-        })
+        }
       }
 
       // Si no existe, crear una nueva entrada
